b4d9858e7970d6bf6198461b06fd441a84596a7d
anjiyuan <anjiyuan@yahoo.com>
2022-11-02 15:39:09 +1000

    1st commit

--- Citrus.bat
+++ Citrus.bat
@@ -1,3 +1,3 @@
 
 
-java -cp C:\Jiyuan\sourceCode\maven\Citrus\target\Citrus-1.0.jar;C:\Jiyuan\sourceCode\maven\Citrus\target\lib\* org.qut.citrus.Citrus
+java -cp target\Citrus-1.0.jar;target\lib\* org.qut.citrus.Citrus
